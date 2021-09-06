d176088ea8780f01f71e5691d82174437111efc9
wangyanlong1994 <wangyanlong1994@gmail.com>
2021-09-06 16:38:23 +0800

    redis消息队列

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\PostViewed;
 use App\Jobs\PostViewsIncrement;
 use App\Models\Post;
 use App\Repos\PostRepo;
@@ -25,7 +26,9 @@ class PostController extends Controller
 //        return 'Show Post #' . $post->id . ', Views:' . $views;
         $post = $this->postRepo->getById($id);
         // 分发队列任务
-        $this->dispatch(new PostViewsIncrement($post));
+//        $this->dispatch(new PostViewsIncrement($post));
+        // 触发文章浏览事件
+        event(new PostViewed($post));
         return "Show Post #{$post->id}, Views: {$post->views}";
     }
 
