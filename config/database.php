f5ce5d830a79421eac10899e8ae1e37ee833a8a0
wangyanlong1994 <wangyanlong1994@gmail.com>
2021-10-25 18:26:50 +0800

    finish

--- config/database.php
+++ config/database.php
@@ -123,16 +123,16 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
+//        'default' => [
+//            'url' => env('REDIS_URL'),
+//            'host' => env('REDIS_HOST', '127.0.0.1'),
+//            'password' => env('REDIS_PASSWORD', null),
+//            'port' => env('REDIS_PORT', '6379'),
+//            'database' => env('REDIS_DB', '0'),
+//        ],
 
         'cache' => [
             'url' => env('REDIS_URL'),
@@ -140,6 +140,47 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => '192.168.65.4',
+                    'password' => null,
+                    'port' => 8011,
+                    'database' => 0,
+                ],
+                [
+                    'host' => '192.168.65.4',
+                    'password' => null,
+                    'port' => 8012,
+                    'database' => 0,
+                ],
+                [
+                    'host' => '192.168.65.4',
+                    'password' => null,
+                    'port' => 8013,
+                    'database' => 0,
+                ],
+                [
+                    'host' => '192.168.65.4',
+                    'password' => null,
+                    'port' => 8014,
+                    'database' => 0,
+                ],
+                [
+                    'host' => '192.168.65.4',
+                    'password' => null,
+                    'port' => 8015,
+                    'database' => 0,
+                ],
+                [
+                    'host' => '192.168.65.4',
+                    'password' => null,
+                    'port' => 8016,
+                    'database' => 0,
+                ]
+            ]
         ],
 
     ],
